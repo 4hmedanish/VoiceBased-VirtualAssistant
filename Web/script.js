7ec123a0cc6ae50d2cf0ca0974ee5965ac176c1c
Danish/ <160564852+4hmedanish@users.noreply.github.com>
2024-12-29 10:50:57 +0530

    Delete Web directory

--- Web/script.js
+++
@@ -1,8 +0,0 @@
-function startListening() {
-    eel.startListening(); // Call the Python function to start listening
-}
-
-eel.expose(updateStatus);
-function updateStatus(statusText) {
-    document.getElementById("status").innerText = "Status: " + statusText;
-}
